c5b6f411258e2a0b97502418b7905a3b394b8966
samuelmelo98 <samuelmelo98@hotmail.com>
2024-12-25 17:15:36 -0300

    Adicionando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PessoaComponent } from '../components/pessoa.component/pessoa.component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'pessoas', component: PessoaComponent }, // Configura a rota para 'pessoas' para o MeuComponente
+    { path: 'teste', redirectTo: '/pessoas', pathMatch: 'full' }, // Redireciona para 'pessoas' por padrão
+    { path: 'samuel', redirectTo: '/', pathMatch: 'full' }, // Redireciona para 'pessoas' por padrão
+    { path: '', redirectTo: '/', pathMatch: 'full' }, // Redireciona para 'pessoas' por padrão
+];
